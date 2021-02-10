b13bf65461584385a7f5e44c35b4451a69071554
t-skoro <65133778+t-skoro@users.noreply.github.com>
2021-02-10 21:53:35 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        nav: true,
+        loop: true,
+        navSpeed: 1000,
+        navText: ["", ""]
+    });
+  });
